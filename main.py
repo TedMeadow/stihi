964303b94d80f78348ebb0822f71efa4f6784a9c
tedmeadow <fl18524a@gmail.com>
2021-09-07 20:49:06 +0300

    v1.0

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from requests.api import request
 import re
+import os
 
 URL = 'https://stihi.ru'
 
@@ -15,22 +16,36 @@ def poem(url):
     poem = soup.find('div', class_='text')
     return (poem_name.text,poem.text)
 
-def books(url):
+def book(url, bookLink, page = 0, poemLinks = {}):
 
-    response = requests.get(url)
+    response = requests.get(url + '&s={0}'.format(page) + '&{0}'.format(bookLink))
     soup = BeautifulSoup(response.text, 'lxml')
     poems = soup.find_all('a', class_='poemlink')
-    poem_links = []
 
     for poem in poems:
-        poem_link = poem.get('href')
-        poem_links.append(poem_link)
+        poemLink = poem.get('href')
+        poemName = poem.text
+        poemLinks.update({poemName: poemLink})
 
-    return poem_links
+    if len(poems) != 0:
+        poemLinks = book(url, page + 50, poemLinks)
+
+    return poemLinks
 
 def author(url, page = 0):
 
-    poems = authorPage
+    poems = authorPage(url)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    books = soup.find_all('div', id='bookheader')
+    authorName = soup.find('h1').text
+    for b in books:
+        bookl = b.find('a').get('href')
+        bookLink = re.match(r'/avtor/.+&(.+)', bookl)
+        poems.update(book(url, bookLink))
+    return (authorName, poems)
+        
+    
 
 def authorPage(url, page = 0, poemLinks = {}):
 
@@ -42,16 +57,22 @@ def authorPage(url, page = 0, poemLinks = {}):
         poemName = poem.text
         poemLinks.update({poemName: poemLink})
     
+    if len(poems) != 0:
+        poemLinks = authorPage(url, page + 50, poemLinks)
+
     return poemLinks
     
 
     
+PATH = 'D:/stihi'
 
 
+authorpg = author('https://stihi.ru/avtor/budarin')
+if not os.path.exists('{0}/{1}'.format(PATH, authorpg[0])):
+    os.mkdir('{0}/{1}'.format(PATH, authorpg[0]))
 
-#urls = author_page('https://stihi.ru/avtor/budarin')
-#for url in urls:
-#    pm = poem(URL + url)
-#    file = open(pm[0], 'w')
-#    file.write(pm[1])
-#
+for name in authorpg[1]:
+    pm = poem(URL + authorpg[1][name])
+    file = open('{0}/{1}/'.format(PATH, authorpg[0]) +re.sub('[/|\:*?<>]','',pm[0]) + '.txt', 'w', encoding='UTF-8')
+    file.write(pm[1])
+    file.close()
